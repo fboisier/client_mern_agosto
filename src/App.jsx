e74a3a95e37011bbf60aea42fb064212cda9524f
Francisco Boisier <fboisier@codingdojo.la>
2024-11-04 21:56:46 -0300

    version inicial

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Menu from "./pages/Menu"
+import { Routes, Route } from "react-router-dom"
+import Inicio from "./pages/Inicio"
+import NotFound from "./pages/NotFound"
+import Login from "./pages/Login"
+import PublicRoutes from "./components/PublicRoutes"
+import PrivateRoutes from "./components/PrivateRoutes"
+import Registro from "./pages/Registro"
+
+const App = () => {
+
+  return (
+    <>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/login" element={
+            <PublicRoutes>
+              <Login />
+            </PublicRoutes>
+          } />
+          <Route path="/registro" element={
+            <PublicRoutes>
+              <Registro />
+            </PublicRoutes>
+          } />
+          <Route path="/" element={
+            <PrivateRoutes>
+              <Menu />
+              <Inicio />
+            </PrivateRoutes>
+          }/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
